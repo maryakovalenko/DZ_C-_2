3173ecedf159a9e492c9169145fac1d75762de62
maryakovalenko <pomashka18061992@gmail.com>
2022-07-17 18:06:31 +0300

    Program.cs

---
+++ DZ_002_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Массивы и функции в программировании
+
+Console.WriteLine("Задача № 13");
+void hasThirdNumber(int x) {
+    int div = 1;
+    while ( x / div >= 10) {
+        div *= 10;
+    }
+    if (div < 100) {
+        Console.WriteLine("Меньшее число");
+    } else {
+        //last number of x / (div/100)    123456
+        int digit = (x / (div/100))  % 10;
+        Console.WriteLine("Third is {0}", digit);      
+    }
+}
+
+ hasThirdNumber(645);
+ hasThirdNumber(78);
+ hasThirdNumber(32679);
+
+       
+    
+
