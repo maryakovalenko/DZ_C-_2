31c9aa8a1ef909ad33e0d75744847c5baf64f6f0
maryakovalenko <pomashka18061992@gmail.com>
2022-07-17 18:04:37 +0300

    Program.cs

---
+++ DZ_002_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Массивы и функции в программировании
+
+Console.WriteLine("Задача № 15");
+Console.WriteLine("--------------------------------");
+
+string [] day = {"Понедельник", "Вторник", "Среда","Четверг","Пятница","Суббота","Воскресенье"};
+
+Console.Write("Вводите целое число (1-7) : ");
+int number = Convert.ToInt32(Console.ReadLine( ));
+
+if (number >= 1 && number <= 7)
+{
+    Console.Write(" Это " + day[number - 1]);
+if ( number==6 || number==7)
+{
+    Console.Write(" - Выходной день.");
+}
+}
+else
+{
+    Console.WriteLine("Этого дня не существует!!!");
+}
